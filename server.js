97ab99ed3fbed347037d2d7e1288ff0598c22769
unknown <kumarnippu000006@gmail.com>
2024-06-14 22:05:15 +0530

    cors

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ import { app } from "./app.js";
 import { connectDB } from "./data/database.js";
 
 connectDB();
-console.log("PORT:", process.env.PORT);
+// console.log("PORT:", process.env.PORT);
 app.listen(process.env.PORT, () => {
-	console.log("server is working");
+	console.log(
+		`server is working on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode`
+	);
 });
--- utils/featurs.js
+++ utils/featurs.js
@@ -1,13 +1,14 @@
 import jwt from "jsonwebtoken";
 export const sendCookie = (user, res, message, statusCode = 200) => {
 	const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+	// console.log(process.env.NODE_ENV == "Development");
 	res
 		.status(statusCode)
 		.cookie("token", token, {
 			httpOnly: true,
 			maxAge: 50 * 60 * 1000,
-			/* sameSite: "none",
-			secure: true, */
+			sameSite: process.env.NODE_ENV == "Development" ? "lax" : "none",
+			secure: process.env.NODE_ENV == "Development" ? false : true,
 		})
 		.json({
 			success: true,
